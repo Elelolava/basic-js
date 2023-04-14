a7b0a9a4f0926fa94c4156783a36189c500614ba
Elelolava <125636205+Elelolava@users.noreply.github.com>
2023-04-14 15:02:45 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(data) {
+  if (!Array.isArray(data)) throw new Error("'arr' parameter must be an instance of the Array!");
+  let index;
+  if (data.some(item => item == '--discard-next')) {
+    if (data.indexOf('--discard-next') == data.length - 1) {
+      index = data.indexOf('--discard-next');
+      data.splice(index, 1);
+    } else {
+      index = data.indexOf('--discard-next') + 1;
+      data.splice(index, 1);
+      data.splice(data.indexOf('--discard-next'), 1);
+    }
+  }
+  if (data.some(item => item == '--discard-prev')) {
+    if (data.indexOf('--discard-prev') == 0) {
+      index = 0;
+      data.splice(index, 1);
+    } else {
+      index = data.indexOf('--discard-prev') - 1;
+      data.splice(index, 1);
+      data.splice(data.indexOf('--discard-prev'), 1);
+    }
+  }
+  if (data.some(item => item == '--double-next')) {
+    if (data.indexOf('--double-next') == data.length - 1) {
+      index = data.indexOf('--double-next');
+      data.splice(index, 1);
+    } else {
+      index = data.indexOf('--double-next') + 1;
+      data.splice(index, 0, data[index]);
+      data.splice(data.indexOf('--double-next'), 1);
+    }
+  }
+  if (data.some(item => item == '--double-prev')) {
+    if (data.indexOf('--double-prev') == 0) {
+      index = 0;
+      data.splice(index, 1);
+    } else {
+      index = data.indexOf('--double-prev') - 1;
+      data.splice(index, 0, data[index]);
+      data.splice(data.indexOf('--double-prev'), 1);
+    }
+  }
+  return data;
 }
 
 module.exports = {
